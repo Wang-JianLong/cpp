627890ab2e62256cbf90844724742e2e50443af5
wangjianlong <1939368045@qq.com>
2023-02-04 12:41:23 +0800

    0.2.1

--- chapter4-composite-type/07_pointer/01_var.cpp
+++ chapter4-composite-type/07_pointer/01_var.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-int main() {
+int main()
+{
   long l = 10L;
   int i = 10;
-  long* lp = &l;
-  int* ip = &i;
+  long *lp = &l;
+  int *ip = &i;
   std::cout << "i = " << i << "  addr = " << ip << std::endl;
   std::cout << "l = " << l << "  addr = " << lp << std::endl;
   return 0;
